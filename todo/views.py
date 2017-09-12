2f9e1e4391d36d9e0f5a6d38afff849e5702b612
John VanSchultz <johnvanschultz@upserve.com>
2017-09-11 21:02:37 -0400

    Add todo index

--- todo/views.py
+++ todo/views.py
@@ -2,10 +2,18 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.template import loader
 
 from django.http import HttpResponse
+
+from .models import Todo
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello world!")
+    todos = Todo.objects.all()
+    template = loader.get_template('todos/index.html')
+    context = {
+        'todos': todos
+    }
+    return HttpResponse(template.render(context, request))
